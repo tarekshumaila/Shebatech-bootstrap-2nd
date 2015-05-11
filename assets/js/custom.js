f40f7fe7751d76f7d88ed5004afeb3a14ee1bde3
tarekshumaila <shebatech@Dhaifs-iMac.local>
2015-05-11 12:32:10 -0600

    more updates
    revisions prior to publishing
    
    — added bootstrap module for get a quote now

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -54,6 +54,21 @@ jQuery(document).ready(function(){
 	jQuery('#portfolio-section').parallax("50%", 0.1);
 	jQuery('#price-section').parallax("50%", 0.3);
 	jQuery('#contact-section ').parallax("50%", 0.1);
+
+	 $("input#submit").click(function(){
+        $.ajax({
+            type: "POST",
+            url: "process.php", //process to mail
+            data: $('form.contact').serialize(),
+            success: function(msg){
+                $("#thanks").html(msg) //hide button and show thank you
+                $("#form-content").modal('hide'); //hide popup  
+            },
+            error: function(){
+                alert("failure");
+            }
+        });
+    });
 })
 //  COMMENT 
 
